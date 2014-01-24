d81beebcc80aa6544baf0a4eebcacfff4b085a61
chamian10 <damianminniti@hotmail.com>
2014-01-24 15:25:04 -0300

    Probar heroku 1

--- config/environment.rb
+++ config/environment.rb
@@ -10,16 +10,16 @@ Tesis::Application.initialize!
 
 
 #These settings are for the sending out email for active admin and consequently the   devise mailer
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.perform_deliveries = true
-ActionMailer::Base.raise_delivery_errors = true
-ActionMailer::Base.smtp_settings =
-    {
-
-        :address            => 'smtp.gmail.com',
-        :port               => 587,
-        :domain             => 'gmail.com', #you can also use google.com
-        :authentication     => :plain,
-        :user_name          => 'damianminniti@gmail.com',
-        :password           => 'mignini10'
-    }
+#ActionMailer::Base.delivery_method = :smtp
+#ActionMailer::Base.perform_deliveries = true
+#ActionMailer::Base.raise_delivery_errors = true
+#ActionMailer::Base.smtp_settings =
+#    {
+#
+#        :address            => 'smtp.gmail.com',
+#        :port               => 587,
+#        :domain             => 'gmail.com', #you can also use google.com
+#        :authentication     => :plain,
+#        :user_name          => 'damianminniti@gmail.com',
+#        :password           => 'mignini10'
+#    }
